205943b158ca33106995f00b99857772f6f7213f
Alberto Barradas <abcsds@gmail.com>
2024-08-02 15:13:22 -0600

    Comment LSL out
    Remove LSL connection.
    Remove external code
    Rename main experiment

--- src/before_experiment.py
+++
@@ -1,6 +0,0 @@
-from pylsl import StreamInfo, StreamOutlet
-
-# Setup LSL outlet
-info = StreamInfo("Psychopy", "Markers", 1, 0, "string", "PsychopyUUID0001")
-outlet = StreamOutlet(info)
-outlet.push_sample(["begin_experiment"])
